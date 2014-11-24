d7a7c71d50be3c23f3b2935ebfca791e8d9d7152
lyhsshi <>
2014-11-24 21:20:53 -0200

    Arquivo de teste

---
+++ src/selenium/Selenium.java
@@ -0,0 +1,31 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ *
+ * @author Aline
+ */
+public class Selenium {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //abre firefox
+        WebDriver driver = new FirefoxDriver();
+        
+        //acessa um site
+        driver.get("http://amavi.org.br");
+        
+        //digita no campo com o a pesquisa
+        WebElement campoDeTexto = driver.findElement(By.name("s"));
+        campoDeTexto.sendKeys("UNIDAVI");
+        
+        //submete form
+        campoDeTexto.submit();
+    }
+}
